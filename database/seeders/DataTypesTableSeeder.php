fc4f2823f29b8c59ad49f9cc8f928521e3362269
Ramakant <gangwar.ramakant@gmail.com>
2022-04-05 18:27:40 +0530

    💄 Icon

--- database/seeders/DataTypesTableSeeder.php
+++ database/seeders/DataTypesTableSeeder.php
@@ -18,7 +18,7 @@ class DataTypesTableSeeder extends Seeder
                 'name'                  => 'relatables',
                 'display_name_singular' => __('joy-voyager-bread-relatable::seeders.data_types.relatable.singular'),
                 'display_name_plural'   => __('joy-voyager-bread-relatable::seeders.data_types.relatable.plural'),
-                'icon'                  => 'voyager-bread',
+                'icon'                  => 'voyager-bread voyager-bread-relatable voyager-magnet',
                 'model_name'            => 'Joy\\VoyagerBreadRelatable\\Models\\Relatable',
                 // 'policy_name'           => 'Joy\\VoyagerBreadRelatable\\Policies\\RelatablePolicy',
                 // 'controller'            => 'Joy\\VoyagerBreadRelatable\\Http\\Controllers\\VoyagerBreadRelatableController',
--- database/seeders/MenuItemsTableSeeder.php
+++ database/seeders/MenuItemsTableSeeder.php
@@ -28,7 +28,7 @@ class MenuItemsTableSeeder extends Seeder
         if (!$menuItem->exists) {
             $menuItem->fill([
                 'target'     => '_self',
-                'icon_class' => 'voyager-bread voyager-bread-relatable',
+                'icon_class' => 'voyager-bread voyager-bread-relatable voyager-magnet',
                 'color'      => null,
                 'parent_id'  => $parentMenuId,
                 'order'      => ++$maxOrder,
